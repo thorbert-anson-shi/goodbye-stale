72b5d050ddc63dfa9eccfa36d444eeb4b499c237
tobtoby <tobyas2005139@gmail.com>
2024-09-09 15:45:43 +0700

    Completed look of UI when no objects in carousel

--- main/models.py
+++ main/models.py
@@ -1,4 +1,7 @@
+import uuid
 from django.db import models
+
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     class Category(models.TextChoices):
@@ -17,3 +20,12 @@ class Product(models.Model):
 class ProductSuggestion(models.Model):
     name = models.CharField(max_length=128)
     description = models.TextField()
+
+class Order(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    orderedAt = models.DateTimeField(auto_created=True)
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name="order")
+
+class OrderedItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="order")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="order")
--- main/views.py
+++ main/views.py
@@ -4,20 +4,28 @@ from django.views.decorators.http import require_POST
 from django.shortcuts import render
 from .models import *
 
+
 def all_recipes(request):
     recipes = Product.objects.all()
     recipe_list = [recipe for recipe in recipes]
 
     return render(request, "main.html", {"recipes": recipe_list})
 
+
 @require_POST
-@csrf_protect 
+@csrf_protect
 def add_suggestion(request: HttpRequest):
     request_obj = request.POST
     try:
-        ps = ProductSuggestion(name=request_obj.get("name"), description=request_obj.get("description"))
+        ps = ProductSuggestion(
+            name=request_obj.get("name"), description=request_obj.get("description")
+        )
     except:
         pass
     ps.save()
 
-    return HttpResponse("Object successfully created!", status=201)
+    return HttpResponse({"message": "Object successfully created!"}, status=201)
+
+
+def create_order(request: HttpRequest):
+    return HttpResponse("No response yet")
