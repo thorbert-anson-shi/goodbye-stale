fdb20bb4b7791385dde8aea282c0223529af3b95
tobtoby <tobyas2005139@gmail.com>
2024-09-09 16:03:03 +0700

    Turned DEBUG=False and changed view on base URL

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("all_recipes/", all_recipes, name="all_recipes"),
+    path("", all_recipes, name="all_recipes"),
     path("add_suggestion/", add_suggestion, name="add_suggestion"),
-    path("create_order/", create_order, name="create_order")
+    path("create_order/", create_order, name="create_order"),
 ]
